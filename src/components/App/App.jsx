1b3d27f4a36ced52e8654fb0b6c94a5a046e22ac
Andre29839 <133494235+Andre29839@users.noreply.github.com>
2023-09-28 20:49:47 +0300

    Update folders

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,24 @@
-import React from 'react';
-import UserRoutes from './UserRoutes';
+import React, { useEffect } from 'react';
 import { ToastContainer } from 'react-toastify';
+import { useDispatch, useSelector } from 'react-redux';
+import { refreshAuthThunk } from 'redux/registerReducers/registerThunks';
+import UserRoutes from './UserRoutes';
+import {
+  selectIsLogin,
+  selectToken,
+} from 'redux/registerReducers/registerSelector';
 
 function App() {
+  const dispatch = useDispatch();
+  const isLogin = useSelector(selectIsLogin);
+  const token = useSelector(selectToken);
+
+  useEffect(() => {
+    if (!isLogin && token) {
+      dispatch(refreshAuthThunk());
+    }
+  }, [dispatch, isLogin, token]);
+
   return (
     <div>
       <UserRoutes />
