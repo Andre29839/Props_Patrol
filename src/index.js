2e21ff6daae615e057930ef569e9d854990f3117
Andre29839 <133494235+Andre29839@users.noreply.github.com>
2023-09-28 15:12:08 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider>
+    <PersistGate>
+      <BrowserRouter basename="/Props_Patrol">
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
 );
