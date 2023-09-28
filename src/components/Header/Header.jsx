9734c2016ebdc35f8ab40a458822e61293f0da25
Elonka5 <101475428+Elonka5@users.noreply.github.com>
2023-09-28 21:47:43 +0300

    add header & header style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { useMediaQuery } from 'react-responsive';
+// import { selectIsModalLogOut } from 'redux/globalReducers/globalSelectors';
+import { openModalLogout } from 'redux/globalReducers/globalSlice';
+import moneylogo from '../../images/logo.svg';
 
 const Header = () => {
-  return <div>Header</div>;
+  // const isModalShow = useSelector(selectIsModalLogOut);
+  const isMobilesize = useMediaQuery({ query: '(max-width:767px)' });
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch(openModalLogout());
+  };
+
+  return (
+    <>
+      <header>
+        <div>
+          <img src={moneylogo} alt="MoneyGuard Logo" />
+          <p>Money Guard</p>
+        </div>
+        <div>
+          <span>Name</span>
+          <button onClick={handleLogout}>{isMobilesize ? '' : 'Exit'}</button>
+        </div>
+      </header>
+    </>
+  );
 };
 
 export default Header;
---
+++ src/components/Header/Header.styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 12px 20px;
+  background: var(--header-color);
+  box-shadow: var(--header-shadow);
+`;
