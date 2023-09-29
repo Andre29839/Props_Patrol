81c10a056b33ccabde412d0235a70d70e2b74021
VolodymyrFedorov <133118295+VolodymyrFedorov@users.noreply.github.com>
2023-09-29 11:22:44 +0300

    Balance

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,19 +1,50 @@
-import React from 'react';
+import { useMediaQuery } from 'react-responsive';
+import {
+  StyledBalance,
+  StyledBalanceBox,
+  StyledBalanceLabel,
+} from './Balance.styled';
 import { useSelector } from 'react-redux';
-import { selectAuthData } from '../../redux/registerReducers/registerSelector';
+import { selectAuthData } from 'redux/registerReducers/registerSelector';
 
-const Balance = () => {
+export const Balance = () => {
   const balance = useSelector(selectAuthData);
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1279 });
+  const isDesktop = useMediaQuery({ minWidth: 1280 });
   return (
     <div>
-      <p>Your balance</p>
-      <div>
-        <span>₴</span>
+      {isDesktop && (
+        <StyledBalanceBox>
+          <StyledBalanceLabel>Your balance</StyledBalanceLabel>
+          <StyledBalance>
+            <span>₴ </span>
 
-        <p>{balance}</p>
-      </div>
+            {balance ? <>{balance.balance}</> : ''}
+          </StyledBalance>
+        </StyledBalanceBox>
+      )}
+      {isTablet && (
+        <StyledBalanceBox maxW="296px" padL="40px" borR="8px" marB="0px">
+          <StyledBalanceLabel>Your balance</StyledBalanceLabel>
+          <StyledBalance>
+            <span>₴ </span>
+
+            {balance ? <>{balance.balance}</> : ''}
+          </StyledBalance>
+        </StyledBalanceBox>
+      )}
+
+      {isMobile && (
+        <StyledBalanceBox maxW="440px" padL="40px" borR="8px" marB="32px">
+          <StyledBalanceLabel>Your balance</StyledBalanceLabel>
+          <StyledBalance>
+            <span>₴ </span>
+
+            {balance ? <>{balance.balance}</> : ''}
+          </StyledBalance>
+        </StyledBalanceBox>
+      )}
     </div>
   );
 };
-
-export default Balance;
