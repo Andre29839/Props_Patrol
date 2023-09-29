4a0fbb361e84cfd7661dcd31a3fd2cbcf6117d5f
Andrii Nepomniashchyi <133494235+Andre29839@users.noreply.github.com>
2023-09-29 20:17:50 +0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -30,7 +30,6 @@ const Header = () => {
         </WrapHeader>
         <WrapBtn>
           <span>{userData?.username}</span>
-          <div></div>
           <button onClick={handleLogout}>
             <LogoExit />
             {isMobilesize ? '' : 'Exit'}
