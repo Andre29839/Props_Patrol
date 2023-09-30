a80fe85fb50874ceac1e9a7574727d51162635a7
sstfnk3107 <s.stefanyk75@gmail.com>
2023-09-30 23:03:08 +0300

    commitsergio

--- src/components/LoginForm/LoginForm.styled.js
+++ src/components/LoginForm/LoginForm.styled.js
@@ -2,6 +2,7 @@ import { Field, Form } from 'formik';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
 import { IoMdMail, IoMdLock } from 'react-icons/io';
 import styled from 'styled-components';
+import mobile2x from 'images/register-bg/bg-register-mobile@2x.jpg';
 
 export const WrapperForm = styled.div`
   padding: 96px 20px;
@@ -13,6 +14,10 @@ export const WrapperForm = styled.div`
   @media (min-width: 320px) and (max-width: 767.9px) {
     height: 100%;
     width: 100%;
+    border-radius: ${({ theme }) => theme.radii.normal};
+    box-shadow: ${({ theme }) => theme.shadows.primary};
+    background-size: cover;
+    background-image: url(${mobile2x});
   }
   @media (min-width: 768px) {
     padding: 80px 62px;
@@ -20,8 +25,12 @@ export const WrapperForm = styled.div`
   }
   border-radius: ${({ theme }) => theme.radii.normal};
   box-shadow: ${({ theme }) => theme.shadows.primary};
-  backdrop-filter: blur(60px);
-  background: rgba(255, 255, 255, 0.1);
+
+  background: radial-gradient(
+    circle,
+    rgb(64, 46, 155) 5%,
+    rgb(76, 50, 113) 100%
+  );
 `;
 
 export const FormStyled = styled(Form)`
@@ -42,13 +51,16 @@ export const WrapperField = styled.div`
 export const FieldStyled = styled(Field)`
   margin-top: 40px;
   padding-left: 40px;
-  width: 400px;
+  width: 280px;
   height: ${({ theme }) => theme.spacing(9)};
   outline: none;
   border: none;
   border-bottom: 2px solid grey;
   background-color: rgba(255, 255, 255, 0);
-  color: grey;
+  color: rgba(255, 255, 255, 0.6);
+  @media (min-width: 768px) {
+    width: 409px;
+  }
 `;
 
 export const WrapperButton = styled.div`
@@ -62,7 +74,7 @@ export const WrapperButton = styled.div`
 export const EmailIcon = styled(IoMdMail)`
   height: 24px;
 
-  color: grey;
+  color: rgba(255, 255, 255, 0.4);
   position: absolute;
   left: 8px;
   top: 74%;
@@ -80,7 +92,7 @@ export const WrapperIcon2 = styled.div`
 export const PasswordlIcon = styled(IoMdLock)`
   height: 24px;
   width: 24px;
-  color: grey;
+  color: rgba(255, 255, 255, 0.4);
   position: absolute;
   left: 8px;
   top: 74%;
@@ -95,7 +107,7 @@ export const WrapperIcon3 = styled.div`
 export const PasswordlIconLook = styled.div`
   height: 24px;
   width: 24px;
-  color: grey;
+  color: rgba(255, 255, 255, 0.4);
   position: absolute;
   right: 0;
   top: 63%;
