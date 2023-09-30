e920365199a9177924c4d5b6a1b96f415d2cdce6
IT14KR <131267129+IT14KR@users.noreply.github.com>
2023-09-30 14:52:29 +0200

    add commit

--- src/components/Currency/Currency.jsx
+++ src/components/Currency/Currency.jsx
@@ -108,30 +108,19 @@ const Currency = () => {
         <StyledBox maxw="336px">
           {isLoading ? (
             <>
-              <StyledOval
-                height={40}
-                width={40}
-                color="#4a56e2"
-                wrapperStyle={{}}
-                wrapperClass=""
-                visible={true}
-                ariaLabel="oval-loading"
-                secondaryColor="#4a56e2"
-                strokeWidth={4}
-                strokeWidthSecondary={4}
-              />
+              <StyledOval />
             </>
           ) : (
             <>
               <StyledTable>
-                <StyledTableHead padL="20px" marB="8px">
+                <StyledTableHead>
                   <StyledThBox>
                     <StyledTh>Currency</StyledTh>
                     <StyledTh>Purchase</StyledTh>
                     <StyledTh>Sale</StyledTh>
                   </StyledThBox>
                 </StyledTableHead>
-                <StyledTBody padL="20px" gap="12px">
+                <StyledTBody>
                   {currency?.length &&
                     currency.map(el => {
                       return (
@@ -156,33 +145,22 @@ const Currency = () => {
         </StyledBox>
       )}
       {isMobile && (
-        <StyledBox maxw="336px">
+        <StyledBox>
           {isLoading ? (
             <>
-              <StyledOval
-                height={40}
-                width={40}
-                color="#4a56e2"
-                wrapperStyle={{}}
-                wrapperClass=""
-                visible={true}
-                ariaLabel="oval-loading"
-                secondaryColor="#4a56e2"
-                strokeWidth={4}
-                strokeWidthSecondary={4}
-              />
+              <StyledOval />
             </>
           ) : (
             <>
               <StyledTable>
-                <StyledTableHead padL="20px" marB="8px">
+                <StyledTableHead>
                   <StyledThBox>
                     <StyledTh>Currency</StyledTh>
                     <StyledTh>Purchase</StyledTh>
                     <StyledTh>Sale</StyledTh>
                   </StyledThBox>
                 </StyledTableHead>
-                <StyledTBody padL="20px" gap="12px">
+                <StyledTBody>
                   {currency?.length &&
                     currency.map(el => {
                       return (
