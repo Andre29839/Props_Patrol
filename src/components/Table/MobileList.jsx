51efdcb863019796a551893b3ddaadc1f7fdb329
gostraa <mcherkashina77@gmail.com>
2023-10-02 11:15:51 +0200

    reduce

--- src/components/Table/MobileList.jsx
+++ src/components/Table/MobileList.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
-import { selectAllTransactions } from 'redux/transactionsRedusers/transactionsSelectors';
+import {
+  selectAllTransactions,
+  selectTransactionsCategories,
+} from 'redux/transactionsRedusers/transactionsSelectors';
 import { refreshBalanceThunk } from 'redux/registerReducers/registerThunks';
 import {
   deleteTransactionsThunk,
@@ -20,9 +23,11 @@ import {
   EditWrapper,
 } from './MobileList.styled';
 import EditTransactionModal from 'components/ModalEdit/EditTransactionModal';
+import { NoTransactionsText } from 'components/TransactionsList/TransactionsList.styled';
 
 const MobileTransactionList = () => {
   const transactions = useSelector(selectAllTransactions);
+  const categories = useSelector(selectTransactionsCategories);
   const dispatch = useDispatch();
 
   const handleDelete = transactionId => {
@@ -32,19 +37,10 @@ const MobileTransactionList = () => {
       .then(() => dispatch(refreshBalanceThunk()));
   };
 
-  const categoryNames = {
-    'c9d9e447-1b83-4238-8712-edc77b18b739': 'Main expenses',
-    '27eb4b75-9a42-4991-a802-4aefe21ac3ce': 'Products',
-    '3caa7ba0-79c0-40b9-ae1f-de1af1f6e386': 'Car',
-    'bbdd58b8-e804-4ab9-bf4f-695da5ef64f4': 'Self care',
-    '76cc875a-3b43-4eae-8fdb-f76633821a34': 'Child care',
-    '128673b5-2f9a-46ae-a428-ec48cf1effa1': 'Household products',
-    '1272fcc4-d59f-462d-ad33-a85a075e5581': 'Education',
-    'c143130f-7d1e-4011-90a4-54766d4e308e': 'Leisure',
-    '719626f1-9d23-4e99-84f5-289024e437a8': 'Other expenses',
-    '3acd0ecd-5295-4d54-8e7c-d3908f4d0402': 'Entertainment',
-    '063f1132-ba5d-42b4-951d-44011ca46262': 'Income',
-  };
+  const gategoryNamesbyId = categories.reduce((res, category) => {
+    res[category.id] = category.name;
+    return res;
+  }, {});
 
   const formatDate = dateString => {
     const date = new Date(dateString);
@@ -61,7 +57,9 @@ const MobileTransactionList = () => {
     <table>
       <tbody>
         <tr>
-          <td colSpan="7">Your transactions will be here</td>
+          <NoTransactionsText colSpan="7">
+            Your transactions will be here
+          </NoTransactionsText>
         </tr>
       </tbody>
     </table>
@@ -92,10 +90,10 @@ const MobileTransactionList = () => {
                       <Td>{formatDate(item.transactionDate)}</Td>
                       <Td>{transactionSymbol(item.type)}</Td>
                       <Td>
-                        {categoryNames[item.categoryId].length > 9
-                          ? categoryNames[item.categoryId].substring(0, 9) +
+                        {gategoryNamesbyId[item.categoryId].length > 9
+                          ? gategoryNamesbyId[item.categoryId].substring(0, 9) +
                             '...'
-                          : categoryNames[item.categoryId]}
+                          : gategoryNamesbyId[item.categoryId]}
                       </Td>
                       <Td>
                         {item.comment
--- src/components/TransactionsList/TransactionsList.jsx
+++ src/components/TransactionsList/TransactionsList.jsx
@@ -6,7 +6,10 @@ import {
 } from '../../redux/transactionsRedusers/transactionsThunks';
 
 import { refreshBalanceThunk } from 'redux/registerReducers/registerThunks';
-import { selectAllTransactions } from '../../redux/transactionsRedusers/transactionsSelectors';
+import {
+  selectAllTransactions,
+  selectTransactionsCategories,
+} from '../../redux/transactionsRedusers/transactionsSelectors';
 import {
   ButtonDelete,
   NoTransactionsText,
@@ -26,6 +29,8 @@ import EditTransactionModal from 'components/ModalEdit/EditTransactionModal';
 
 const TransactionsList = () => {
   const transactions = useSelector(selectAllTransactions);
+  const categories = useSelector(selectTransactionsCategories);
+
   const dispatch = useDispatch();
 
   const formatDate = dateString => {
@@ -46,19 +51,10 @@ const TransactionsList = () => {
   const transactionSymbol = type =>
     type === 'INCOME' ? '+' : type === 'EXPENSE' ? '-' : '';
 
-  const categoryNames = {
-    'c9d9e447-1b83-4238-8712-edc77b18b739': 'Main expenses',
-    '27eb4b75-9a42-4991-a802-4aefe21ac3ce': 'Products',
-    '3caa7ba0-79c0-40b9-ae1f-de1af1f6e386': 'Car',
-    'bbdd58b8-e804-4ab9-bf4f-695da5ef64f4': 'Self care',
-    '76cc875a-3b43-4eae-8fdb-f76633821a34': 'Child care',
-    '128673b5-2f9a-46ae-a428-ec48cf1effa1': 'Household products',
-    '1272fcc4-d59f-462d-ad33-a85a075e5581': 'Education',
-    'c143130f-7d1e-4011-90a4-54766d4e308e': 'Leisure',
-    '719626f1-9d23-4e99-84f5-289024e437a8': 'Other expenses',
-    '3acd0ecd-5295-4d54-8e7c-d3908f4d0402': 'Entertainment',
-    '063f1132-ba5d-42b4-951d-44011ca46262': 'Income',
-  };
+  const gategoryNamesbyId = categories.reduce((res, category) => {
+    res[category.id] = category.name;
+    return res;
+  }, {});
 
   return (
     <Table>
@@ -87,9 +83,9 @@ const TransactionsList = () => {
                 <TdDate>{formatDate(transactionDate)}</TdDate>
                 <TdType>{transactionSymbol(type)}</TdType>
                 <Td>
-                  {categoryNames[categoryId].length > 10
-                    ? categoryNames[categoryId].substring(0, 10) + '...'
-                    : categoryNames[categoryId]}
+                  {gategoryNamesbyId[categoryId].length > 10
+                    ? gategoryNamesbyId[categoryId].substring(0, 10) + '...'
+                    : gategoryNamesbyId[categoryId]}
                 </Td>
                 <Td>
                   {comment
