0b21c372c256cf9d75f7806abaf8dce4d69f669b
Andre29839 <133494235+Andre29839@users.noreply.github.com>
2023-12-18 14:51:28 +0200

    Update baseURL.js

--- src/services/api/baseURL.js
+++ src/services/api/baseURL.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: `https://wallet.goit.ua/api`,
+  baseURL: `https://wallet.b.goit.study/docs`,
 });
 
 export default instance;
